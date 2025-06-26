27f9df278482bae40332b5fd388cba155689021b
Susanna Berntsson <susanna.berntsson@hoy.se>
2025-06-26 17:29:56 +0200

    add global styles

---
+++ app/globalStyles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    h1: {
+        fontSize: 40,
+        fontWeight: 'bold',
+    },
+    h2: {
+        fontSize: 32,
+        fontWeight: 'bold',
+    },
+    h3: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    h4: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    h5: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    label: {
+        fontSize: 12,
+        fontFamily: 'monospace',
+    },
+    button: {
+        fontSize: 12,
+        fontFamily: 'monospace',
+        fontWeight: 'bold',
+    }
+});
+
+export default styles;
